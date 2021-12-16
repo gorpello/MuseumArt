2a41acaf240d6bfce63ddce5c4943a9c0207b6c6
Gianluca Orpello <g.orpello@gmail.com>
2021-12-16 21:38:44 +0100

    Initial Commit

---
+++ MuseumArt/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MuseumArt
+//
+//  Created by Gianluca Orpello for the Developer Academy on 16/12/21.
+//
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuseumArt/MuseumArtApp.swift
@@ -0,0 +1,19 @@
+//
+//  MuseumArtApp.swift
+//  MuseumArt
+//
+//  Created by Gianluca Orpello for the Developer Academy on 16/12/21.
+//
+//
+
+
+import SwiftUI
+
+@main
+struct MuseumArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
