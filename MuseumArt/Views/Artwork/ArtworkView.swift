2f01451029e47e29e50ae2cf3ffe8d7d60975856
Gianluca Orpello <g.orpello@gmail.com>
2021-12-16 23:56:42 +0100

    First Version

--- MuseumArt/Views/Artwork/ArtworkView.swift
+++ MuseumArt/Views/Artwork/ArtworkView.swift
@@ -10,14 +10,33 @@
 import SwiftUI
 
 struct ArtworkView: View {
+        
     var body: some View {
         NavigationView{
-            Text("Hello, world!")
-                .padding()
-                .navigationTitle(Text("Artwork"))
+            TabView {
+                resizedImage(with: "CollezioneAvalos")
+                resizedImage(with: "CollezioneDeCiccio")
+                resizedImage(with: "CollezioneFarnese")
+                resizedImage(with: "capodimonte")
+                resizedImage(with: "ManifestiMele")
+            }
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
+            
+            .navigationTitle(Text("Artwork"))
         }
     }
+    
+    func resizedImage(with name: String) -> some View {
+        Image(name)
+            .resizable()
+            .aspectRatio(3/2, contentMode: .fit)
+            .frame(width: UIScreen.main.bounds.width - 38)
+            .border(.blue, width: 10)
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- MuseumArt/Views/CustomCollection/CustomCollectionView.swift
+++ MuseumArt/Views/CustomCollection/CustomCollectionView.swift
@@ -10,11 +10,41 @@
 import SwiftUI
 
 struct CustomCollectionView: View {
+    
+    
     var body: some View {
+        
         NavigationView{
-            Text("Hello, World!")
-                .navigationTitle(Text("La Tua Collezione"))
+            
+            ScrollView {
+                
+                Image("Gianluca")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(lineWidth: 8)
+                            .foregroundColor(.yellow)
+                    )
+                    .shadow(radius: 10)
+                    .padding()
+                
+                // Name and Surname (Title)
+                Text("Gianluca The Bug Maker")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.pink)
+                    .padding(.bottom)
+                
+                // Description (Body)
+                Text("I create bugs in the code I implement, even if the project sometimes works...")
+            }
+            
+            .padding()
+            .navigationTitle(Text("La Tua Collezione"))
         }
+        
     }
 }
 
